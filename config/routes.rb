9cd26cfe480b58fa8be82bd43db62b0f967a8ad2
Rodrigo de Benito Sanz <rodrigo.debenitosanz@nytimes.com>
2015-03-30 21:19:07 -0400

    Adds route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'sessions#new'
   resources :sessions, only: :index
+  get "/upload" => 'sessions#upload'
   get "/list" => 'sessions#list'
   get "/auth/:provider/callback" => 'sessions#create'
 end
